731a0f7c3ccbecfb7045fd63af5c70e1d0cf0682
Bruno Moreno <brunomorello7@gmail.com>
2020-01-21 13:21:11 -0300

    app architecture changed

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,27 +1,51 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { CourseService } from "../../services/course.service";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-    getSpecializationList(event: Event) {
-        console.log(event);
-        // console.log(this.courseSpecializationList.filter(element => element.courseId == courseId));
-        // return this.courseSpecializationList.filter(element => element.courseId == courseId);
+    mainFormSearch = new FormGroup({
+        postGraduation: new FormControl(true),
+        mba: new FormControl(),
+        shortDuration: new FormControl(),
+        mainCourse: new FormControl(),
+        courseSpecialization: new FormControl(),
+        onSiteCourse: new FormControl(true),
+        semiOnSiteCourse: new FormControl(true),
+        onlineCourse: new FormControl()
+    });
+
+    private courseList = [];
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit() {
+        
+        // this.courseService.getCourseList()
+        //     .subscribe(courseList => {
+        //         this.courseList = courseList;
+        //     });
+
+    }
+
+    getSpecializationList() {
+        
+        console.log(this.mainFormSearch.get('mainCourse'));
     
       }
     
-      performSearch(formCourseSearch: NgForm) {
-    
-        console.log(formCourseSearch);
-    
-        if(formCourseSearch.invalid) return;
-    
-        formCourseSearch.reset();
+      performSearch() {
+
+        if (this.mainFormSearch.valid) {
+            console.log(this.mainFormSearch.value);
+        } else {
+            console.log(`Fillout all fields`);
+        }
     
       }
 }
