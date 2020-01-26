9e903884499b2a153d9bb9c422dd2217e16160da
Bruno Moreno <brunomorello7@gmail.com>
2020-01-26 00:38:15 -0300

    added Input Binding for select inputs

--- src/app/modules/search-course/search-course.component.ts
+++ src/app/modules/search-course/search-course.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CourseSearchApiResponse } from 'src/app/models/CourseSearchApiResponse';
+import { Course } from 'src/app/models/Course';
+import { Specialization } from 'src/app/models/Specialization';
 
 @Component({
   selector: 'app-search-course',
@@ -11,5 +13,11 @@ export class SearchCourseComponent {
   @Input() menuSearchOptions;
 
   @Input() searchCouseResult: CourseSearchApiResponse;
+
+  @Input() courseList: Course[];
+
+  @Input() specializationListDump: Specialization[];
+
+  @Input() specializationList: Specialization[];
 
 }
