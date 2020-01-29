53cb86b2b180b05f5c24631fcb7d47cf2a4ab6ff
Bruno Moreno <brunomorello7@gmail.com>
2020-01-29 00:15:21 -0300

    two-way data binding and validations added

--- src/app/modules/home-customer-doubts-form-email/home-customer-doubts-form-email.component.ts
+++ src/app/modules/home-customer-doubts-form-email/home-customer-doubts-form-email.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,18 +6,66 @@ import { NgForm } from '@angular/forms';
   templateUrl: './home-customer-doubts-form-email.component.html',
   styleUrls: ['./home-customer-doubts-form-email.component.css']
 })
-export class HomeCustomerDoubtsFormEmailComponent implements OnInit {
+export class HomeCustomerDoubtsFormEmailComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  customer = {
+    name: '',
+    email: '',
+    age: '0',
+    educationLevel: 'Ensino médio completo',
+    employed: false,
+    currentJob: '',
+    investmentIntention: '0',
+    courseType: 'Presencial',
+    aspiration: '',
+    agreeReceiveEmails: false
+  }
+
+  checkChanges(element) {
+
+    let domElement = $(`#${element}`);
+
+    if (domElement.val() != "" || domElement.val() != "0") {
+      domElement.removeClass('is-invalid');
+      domElement.addClass('is-valid');
+    }
+
   }
 
   sendForm(customerDoubtsForm: NgForm) {
 
-    console.log(customerDoubtsForm);
-    return;
+    let domElementList = [
+      $("#customer-doubts-form-cust-name"),
+      $("#customer-doubts-form-cust-email"),
+      $("#customer-doubts-form-cust-age"),
+      $("#customer-doubts-form-cust-current-job"),
+      $("#customer-doubts-form-cust-investment")
+    ];
 
+    let invalidField = false;
+
+    // validation of empty elements
+    domElementList.forEach(element => {
+
+      console.log(element.val());
+
+      if (element.val() == "" || element.val() == "0" || element.val() == null) {
+        element.addClass('is-invalid');
+        invalidField = true;
+        console.log(element);
+      }
+    });
+
+    if(invalidField) return;
+
+    console.log(customerDoubtsForm);
+
+  }
+
+  removeValidations() {
+    
   }
 
 }
