169bcc1b6277d5fbfcdf0df3a4167b232c003ded
Bruno Moreno <brunomorello7@gmail.com>
2020-01-29 20:48:38 -0300

    changed method to return an observable

--- src/app/services/send-grid-mail-api.service.ts
+++ src/app/services/send-grid-mail-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -9,42 +9,10 @@ export class SendGridMailAPIService {
 
   constructor(private http: HttpClient) { }
 
-  private url = "https://api.sendgrid.com/v3/mail/send";
-  private token = "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";
+  private url = "http://www.direkte.com.br:10009/mail/sendMail";
 
-  sendMail(customerForm: any): Observable<Object> {
-
-    let httpHeaders = {
-      headers: new HttpHeaders({
-        'Authorization': `Bearer ${this.token}`,
-        'Content-Type': 'application/json; charset=utf-8',
-        'Accept': '*/*'
-      })
-    };
-
-    let message = {
-      "personalizations": [
-        {
-          "to": [
-            {"email": "brunomorello7@gmail.com"}
-          ]
-        }
-      ],
-      "from": {
-        "email": "brunomorello7@gmail.com"
-        
-      },
-      "subject": "Sending with SendGrid is Fun",
-      "content": [
-        {
-          "type": "text/plain", 
-          "value": customerForm
-        }
-      ]
-    }
-
-    return this.http.post(this.url, message, httpHeaders);
-
+  sendMail(message: any): Observable<any> {
+    return this.http.post(this.url, message);
   }
 
 }
