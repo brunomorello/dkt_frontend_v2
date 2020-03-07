856202e2a5db7fdd541d3ce048357f1df0e03405
Bruno Moreno <brunomorello7@gmail.com>
2020-03-07 17:04:15 -0300

    refactor to reuse httpHeaders and created function to search specific course

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 import { CourseApiResponse } from '../models/CourseApiResponse';
 import { CourseSearchApiResponse } from '../models/CourseSearchApiResponse';
+import { CourseSearchResponse } from '../models/CourseSearchResponse';
+import { CourseSearchByNameApiResponse } from '../models/CourseSearchByNameApiResponse';
 
 @Injectable({
     providedIn: 'root'
@@ -13,6 +16,12 @@ export class CourseService {
 
     private endpoint = "http://direkte.com.br:10009";
 
+    private httpHeaders = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json; charset=UTF-8'
+        })
+    };
+
     getCourseList(): Observable<CourseApiResponse> {
 
         return this.http.get<CourseApiResponse>(`${this.endpoint}/atuacao/listarTodos`);
@@ -21,14 +30,45 @@ export class CourseService {
 
     searchCourse(searchOptions): Observable<CourseSearchApiResponse> {
 
-        const httpHeaders = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json; charset=UTF-8'
-            })
-        };
-    
-        return this.http.post<CourseSearchApiResponse>(`${this.endpoint}/curso/listarTodosPesquisa`, searchOptions, httpHeaders);
+        return this.http.post<CourseSearchApiResponse>(`${this.endpoint}/curso/listarTodosPesquisa`, searchOptions, this.httpHeaders);
 
+    }
+
+    searchCourseByName(courseName: string) {
+
+        let req = {
+            "nomeDoCurso": courseName
+        };
+
+        return this.http.post(`${this.endpoint}/curso/listarPorNome/1`, req, this.httpHeaders)
+            .pipe(
+                map((courseApiResponse: any) => {
+                    
+                    return new CourseSearchResponse({
+                        id: courseApiResponse.msg.cursos[0].id,
+                        nome: courseApiResponse.msg.cursos[0].nome,
+                        valorTotal: courseApiResponse.msg.cursos[0].valorTotal,
+                        valorMensal: courseApiResponse.msg.cursos[0].valorMensal,
+                        periodo: courseApiResponse.msg.cursos[0].periodo,
+                        duracao: courseApiResponse.msg.cursos[0].duracao,
+                        mba: courseApiResponse.msg.cursos[0].mba,
+                        pos: courseApiResponse.msg.cursos[0].pos,
+                        is_active: courseApiResponse.msg.cursos[0].is_active,
+                        id_modalidade: courseApiResponse.msg.cursos[0].id_modalidade,
+                        id_instituicao: courseApiResponse.msg.cursos[0].id_instituicao,
+                        id_cursoUrl: courseApiResponse.msg.cursos[0].id_cursoUrl,
+                        id_cursoUrl_manual: courseApiResponse.msg.cursos[0].id_cursoUrl_manual,
+                        id_especializacao: courseApiResponse.msg.cursos[0].id_especializacao,
+                        id_atuacao: courseApiResponse.msg.cursos[0].id_atuacao,
+                        especializacao: courseApiResponse.msg.cursos[0].especializacao,
+                        atuacao: courseApiResponse.msg.cursos[0].atuacao,
+                        instituicao: courseApiResponse.msg.cursos[0].instituicao,
+                        modalidade: courseApiResponse.msg.cursos[0].modalidade,
+                        cursoUrl: courseApiResponse.msg.cursos[0].cursoUrl,
+                        cursoUrlManual: courseApiResponse.msg.cursos[0].cursoUrlManual
+                    });
+                })
+            )
     }
 
 }
