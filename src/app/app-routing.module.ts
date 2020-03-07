af9b824455ed25ba449e61813ec28fccb46be74b
Bruno Moreno <brunomorello7@gmail.com>
2020-03-07 17:02:18 -0300

    Created Route for Course Search Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./modules/home/home.component";
-import { SearchCourseComponent } from "./modules/search-course/search-course.component";
+import { CourseSearchComponent } from "./components/course-search/course-search.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'courseSearch', component: CourseSearchComponent},
   {path: '**', redirectTo: ''}
 ];
 
