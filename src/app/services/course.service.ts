8260f6318289eae4b2b68d2bea5cb86671f802de
Bruno Moreno <brunomorello7@gmail.com>
2020-03-18 08:59:05 -0300

    function renamed

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -34,38 +34,35 @@ export class CourseService {
 
     }
 
-    searchCourseByName(courseName: string) {
+    searchCourseById(courseId: number) {
 
-        let req = {
-            "nomeDoCurso": courseName
-        };
-
-        return this.http.post(`${this.endpoint}/curso/listarPorNome/1`, req, this.httpHeaders)
+        return this.http.get(`${this.endpoint}/curso/listarPorId/${courseId}`)
             .pipe(
                 map((courseApiResponse: any) => {
+                    console.log(courseApiResponse);
                     
                     return new CourseSearchResponse({
-                        id: courseApiResponse.msg.cursos[0].id,
-                        nome: courseApiResponse.msg.cursos[0].nome,
-                        valorTotal: courseApiResponse.msg.cursos[0].valorTotal,
-                        valorMensal: courseApiResponse.msg.cursos[0].valorMensal,
-                        periodo: courseApiResponse.msg.cursos[0].periodo,
-                        duracao: courseApiResponse.msg.cursos[0].duracao,
-                        mba: courseApiResponse.msg.cursos[0].mba,
-                        pos: courseApiResponse.msg.cursos[0].pos,
-                        is_active: courseApiResponse.msg.cursos[0].is_active,
-                        id_modalidade: courseApiResponse.msg.cursos[0].id_modalidade,
-                        id_instituicao: courseApiResponse.msg.cursos[0].id_instituicao,
-                        id_cursoUrl: courseApiResponse.msg.cursos[0].id_cursoUrl,
-                        id_cursoUrl_manual: courseApiResponse.msg.cursos[0].id_cursoUrl_manual,
-                        id_especializacao: courseApiResponse.msg.cursos[0].id_especializacao,
-                        id_atuacao: courseApiResponse.msg.cursos[0].id_atuacao,
-                        especializacao: courseApiResponse.msg.cursos[0].especializacao,
-                        atuacao: courseApiResponse.msg.cursos[0].atuacao,
-                        instituicao: courseApiResponse.msg.cursos[0].instituicao,
-                        modalidade: courseApiResponse.msg.cursos[0].modalidade,
-                        cursoUrl: courseApiResponse.msg.cursos[0].cursoUrl,
-                        cursoUrlManual: courseApiResponse.msg.cursos[0].cursoUrlManual
+                        id: courseApiResponse.msg[0].id,
+                        nome: courseApiResponse.msg[0].nome,
+                        valorTotal: courseApiResponse.msg[0].valorTotal,
+                        valorMensal: courseApiResponse.msg[0].valorMensal,
+                        periodo: courseApiResponse.msg[0].periodo,
+                        duracao: courseApiResponse.msg[0].duracao,
+                        mba: courseApiResponse.msg[0].mba,
+                        pos: courseApiResponse.msg[0].pos,
+                        is_active: courseApiResponse.msg[0].is_active,
+                        id_modalidade: courseApiResponse.msg[0].id_modalidade,
+                        id_instituicao: courseApiResponse.msg[0].id_instituicao,
+                        id_cursoUrl: courseApiResponse.msg[0].id_cursoUrl,
+                        id_cursoUrl_manual: courseApiResponse.msg[0].id_cursoUrl_manual,
+                        id_especializacao: courseApiResponse.msg[0].id_especializacao,
+                        id_atuacao: courseApiResponse.msg[0].id_atuacao,
+                        especializacao: courseApiResponse.msg[0].especializacao,
+                        atuacao: courseApiResponse.msg[0].atuacao,
+                        instituicao: courseApiResponse.msg[0].instituicao,
+                        modalidade: courseApiResponse.msg[0].modalidade,
+                        cursoUrl: courseApiResponse.msg[0].cursoUrl,
+                        cursoUrlManual: courseApiResponse.msg[0].cursoUrlManual
                     });
                 })
             )
