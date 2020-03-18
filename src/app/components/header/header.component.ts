8745305f1293fe3685c5484365724352bb196082
Bruno Moreno <brunomorello7@gmail.com>
2020-03-18 08:55:41 -0300

    Created promo bar and imported Router to navigate among URLs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
     selector: 'dkt-header',
@@ -10,12 +10,29 @@ import { Router } from "@angular/router";
 })
 export class HeaderComponent {
 
-    constructor(private router: Router) {}
+    constructor(private router: Router) {
+        this.router.routeReuseStrategy.shouldReuseRoute = function(){
+            return false;
+         }
+    
+         this.router.events.subscribe((evt) => {
+            if (evt instanceof NavigationEnd) {
+               // trick the Router into believing it's last link wasn't previously loaded
+               this.router.navigated = false;
+               // if you need to scroll back to top, here is the right place
+               window.scrollTo(0, 0);
+            }
+        });
+    }
 
     @Input()displayMenuItens = false;
 
     goToHome() {
         this.router.navigate(['/']);
-      }
+    }
+
+    goToPromoPage() {
+        this.router.navigate(['/courseSearch'], { queryParams: { promo: 'ti' } });
+    }
 
 }
